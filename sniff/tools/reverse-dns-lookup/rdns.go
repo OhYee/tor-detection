39806bacb9eea85ea82ae1ee49ec519ac78483c5
OhYee <oyohyee@oyohyee.com>
2020-11-13 17:01:11 +0800

    misc: add progress status for rdns(#master)

--- sniff/tools/reverse-dns-lookup/rdns.go
+++ sniff/tools/reverse-dns-lookup/rdns.go
@@ -3,6 +3,7 @@ package main
 //go:generate go build -o ../../bin/rdns rdns.go
 import (
 	"database/sql"
+	"fmt"
 	"net"
 	"strings"
 
@@ -30,9 +31,17 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	ips := make([]string, 0)
 	for rows.Next() {
 		var ip string
 		rows.Scan(&ip)
+		ips = append(ips, ip)
+	}
+
+	l := len(ips)
+	for idx, ip := range ips {
+		fmt.Printf("%d/%d\r", idx, l)
 		conn.Exec("UPDATE `tor`.`ip` SET `domain`=? where `ip`=?", reverseDNSLookup(ip), ip)
 	}
 }
